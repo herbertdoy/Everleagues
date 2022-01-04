a1336366b8636c67c29bb17449da9da4763eb0e0
jettgeniusgiant <jett.geniusgiant@gmail.com>
2022-01-04 21:11:33 +0800

    Updated Side Icons

--- Test Suites/TS-Organization/04-Side Icons.ts
+++ Test Suites/TS-Organization/04-Side Icons.ts
@@ -18,33 +18,27 @@
       <testCaseId>Test Cases/01-Login/04-Login (valid credentials)</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>7dafadd0-1750-456e-b2a0-e1f53a5334fc</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/04-Side Icons (Organization)/TCSI-01 (Local Hospital)</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>c03042b8-8ebe-4ea3-9bcd-130a131d308b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/04-Side Icons (Organization)/TCSI-02 (Add Contact)</testCaseId>
+      <testCaseId>Test Cases/04-Side Icons (Organization)/01- (Add Contact)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>995d19d5-585c-4541-8d4e-e1dbe18e4f15</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/04-Side Icons (Organization)/TCSI-03 (2FA)</testCaseId>
+      <testCaseId>Test Cases/04-Side Icons (Organization)/02-(2FA)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>cb832c6c-90de-4e3a-a1ba-e3a075bebcee</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/04-Side Icons (Organization)/TCSI-04 (Client Assignment)</testCaseId>
+      <testCaseId>Test Cases/04-Side Icons (Organization)/03-(Client Assignment)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c1479c61-c7a0-4d09-a35a-0bd6965f6862</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/04-Side Icons (Organization)/TCSI-05 (Hide Quick Shortcuts)</testCaseId>
+      <testCaseId>Test Cases/04-Side Icons (Organization)/04-(Hide Quick Shortcuts)</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
