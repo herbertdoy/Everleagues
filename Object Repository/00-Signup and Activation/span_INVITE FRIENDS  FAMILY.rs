efa07e503cc8c3aad4a87274593952a7f20c612a
MICHELLE PANGANIBAN <delunakathleen3@gmail.com>
2023-06-15 22:43:47 +0800

    UPDATED 06/15/23

--- Object Repository/00-Signup and Activation/span_INVITE FRIENDS  FAMILY.rs
+++ Object Repository/00-Signup and Activation/span_INVITE FRIENDS  FAMILY.rs
@@ -6,15 +6,15 @@
    <elementGuidId>da2b631b-e89a-44f8-9f99-3370dafb74a3</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
-         <value>//span[(text() = 'INVITE FRIENDS &amp; FAMILY' or . = 'INVITE FRIENDS &amp; FAMILY')]</value>
-      </entry>
-      <entry>
          <key>CSS</key>
       </entry>
       <entry>
          <key>XPATH</key>
-         <value>//span[text()='INVITE YOUR FRIENDS &amp; FAMILY']</value>
+         <value>//span[text()='INVITE FRIENDS &amp; FAMILY']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//span[(text() = 'INVITE FRIENDS &amp; FAMILY' or . = 'INVITE FRIENDS &amp; FAMILY')]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -25,7 +25,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>span</value>
-      <webElementGuid>591a61e3-cefb-4208-a401-978dd900717d</webElementGuid>
+      <webElementGuid>07d7674f-4ec5-4241-8520-9496b335c100</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -33,7 +33,7 @@
       <name>class</name>
       <type>Main</type>
       <value>mat-button-wrapper</value>
-      <webElementGuid>28e72903-9a0d-436e-a844-85f0ad9c1efc</webElementGuid>
+      <webElementGuid>3972d4d7-a1f7-4e08-b16f-fc2711eb7f15</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -41,7 +41,7 @@
       <name>text</name>
       <type>Main</type>
       <value>INVITE FRIENDS &amp; FAMILY</value>
-      <webElementGuid>502977ad-069f-4be8-ade4-36e1be4cf2f8</webElementGuid>
+      <webElementGuid>ac347897-9167-4795-837d-38a15ac09643</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -49,7 +49,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;mat-dialog-0&quot;)/app-dialog-welcome-new-account[@class=&quot;ng-star-inserted&quot;]/section[@class=&quot;dialog-container&quot;]/div[@class=&quot;mat-dialog-content&quot;]/div[1]/div[3]/button[@class=&quot;mat-focus-indicator mat-button mat-button-base mat-primary&quot;]/span[@class=&quot;mat-button-wrapper&quot;]</value>
-      <webElementGuid>4b59c4d1-11cb-408b-a941-1fb9bfe87fe4</webElementGuid>
+      <webElementGuid>1099be8b-8a70-488a-9d32-cd84937a6e84</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -57,7 +57,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//mat-dialog-container[@id='mat-dialog-0']/app-dialog-welcome-new-account/section/div/div/div[3]/button/span</value>
-      <webElementGuid>4fd19e1c-5cff-4d7e-ad11-548c995c6b34</webElementGuid>
+      <webElementGuid>f10f3896-eead-440f-bcd6-317d454aa773</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -65,7 +65,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='CREATE A BUSINESS ORGANIZATION'])[1]/following::span[1]</value>
-      <webElementGuid>30211140-83d9-40a3-81b2-639fed2ec29a</webElementGuid>
+      <webElementGuid>1c8d1901-56b1-45c2-87b9-b9ae67a8c555</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -73,7 +73,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Business Organization'])[1]/following::span[2]</value>
-      <webElementGuid>88871ef5-3234-4bd3-93af-b7b5401b5eca</webElementGuid>
+      <webElementGuid>53f2cb40-6034-4467-b835-57b11068a8f0</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -81,7 +81,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='DO THIS LATER'])[1]/preceding::span[1]</value>
-      <webElementGuid>0cb97c2d-cc13-43df-816e-debdd6dd47af</webElementGuid>
+      <webElementGuid>eb07daa1-1ebd-4a3b-bcbc-8bab7d65b010</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -89,7 +89,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Invite New User'])[1]/preceding::span[3]</value>
-      <webElementGuid>243601b3-78d2-448b-b7cf-1590ac575314</webElementGuid>
+      <webElementGuid>bd5c8108-5f10-4f29-8ff3-d4007ae7e3fa</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -97,7 +97,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>//*/text()[normalize-space(.)='INVITE FRIENDS &amp; FAMILY']/parent::*</value>
-      <webElementGuid>01ad74e9-5209-4e47-8f05-a66481119a08</webElementGuid>
+      <webElementGuid>6755dcdc-48fc-4366-8ff3-2b4b60dccd96</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -105,7 +105,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div/div/div[3]/button/span</value>
-      <webElementGuid>1b9498a9-3ff1-419a-ab89-2fa9588c3254</webElementGuid>
+      <webElementGuid>650e4765-6b47-4f64-8821-4a9e9df36fae</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -113,6 +113,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//span[(text() = 'INVITE FRIENDS &amp; FAMILY' or . = 'INVITE FRIENDS &amp; FAMILY')]</value>
-      <webElementGuid>c4fc3ca4-5984-4328-9939-1eb6ca83aaf7</webElementGuid>
+      <webElementGuid>a7dafc6e-ccde-4bea-8ddc-3929ab0b6625</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
