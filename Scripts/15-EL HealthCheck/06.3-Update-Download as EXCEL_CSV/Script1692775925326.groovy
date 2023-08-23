d2a936755e389122055e7731fb4a7201b0c15ae9
JohnFet21 <113165546+kathGeniusGiant@users.noreply.github.com>
2023-08-23 22:19:53 +0800

    Update 8/23/2023

---
+++ Scripts/15-EL HealthCheck/06.3-Update-Download as EXCEL_CSV/Script1692775925326.groovy
@@ -0,0 +1,42 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import java.io.File
+
+WebUI.click(findTestObject('15-EL HealthCheck/Activated information tracker/UPDATE/Update_Download as button'))
+
+WebUI.click(findTestObject('15-EL HealthCheck/Activated information tracker/UPDATE/Update_DownloadAs_EXCELCSV'))
+
+WebUI.click(findTestObject('15-EL HealthCheck/Activated information tracker/UPDATE/Update_download_button'))
+
+
+// Assertion Steps to see if the file exists on downloads path
+def static boolean verifyFileExists(String filePath) {
+	File file = new File(filePath)
+	return file.exists() && !file.isDirectory()
+}
+
+// Verify if the file exists
+def filePath = "C:\\Users\\Personal\\Downloads\\InfoTrackerCSV.csv"
+boolean fileExists = verifyFileExists(filePath)
+
+// Perform an assertion based on the result
+assert fileExists : "File exists at path: " + filePath
+
+
+
